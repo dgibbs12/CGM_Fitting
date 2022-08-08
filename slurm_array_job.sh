ae5ec424284a4fe4143bdf2c38b4cc89c91d5f5b
Drew Gibbs <65246389+dgibbs12@users.noreply.github.com>
2022-08-08 09:30:19 -0400

    SLURM script to execute slurm_array.py

---
+++ slurm_array_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=slurm_sample_
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=andrew.gibbs@ufl.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4 
+#SBATCH --mem=4gb
+#SBATCH --time=120:00:00
+#SBATCH --array=1-22
+#SBATCH --output=slurm_sample__%A-%a.log
+
+pwd; hostname; date
+module load python
+
+python /home/andrew.gibbs/paul.torrey/andrew.gibbs/Research/CGM_Fitting/slurm_array.py $SLURM_ARRAY_TASK_ID 
+
+date
+
+
+
